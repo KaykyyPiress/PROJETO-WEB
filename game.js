293f9c8376db46c63deb7f8a5a0223fea444ab12
KaykyyPiress <kaykyypiress@gmail.com>
2023-05-20 23:41:39 -0300

    .

--- game.js
+++ game.js
@@ -19,15 +19,17 @@ var canvas = document.getElementById("myCanvas");
 		var brickOffsetTop = 30;
 		var brickOffsetLeft = 30;
 		var score = 0;
+		var ballSpeed = 1; // Aumente esse valor para aumentar a velocidade da bola a cada colisão
+		var brickRowCount = 9; // Aumente o número de linhas de tijolos
+		var brickColumnCount = 10; // Aumente o número de colunas de tijolos
 
 		var bricks = [];
-		for(var c=0; c<brickColumnCount; c++) {
+		for (var c = 0; c < brickColumnCount; c++) {
 			bricks[c] = [];
-			for(var r=0; r<brickRowCount; r++) {
+			for (var r = 0; r < brickRowCount; r++) {
 				bricks[c][r] = { x: 0, y: 0, status: 1 };
 			}
 		}
-
 		document.addEventListener("keydown", keyDownHandler, false);
 		document.addEventListener("keyup", keyUpHandler, false);
 		document.addEventListener("mousemove", mouseMoveHandler, false);
@@ -54,39 +56,40 @@ var canvas = document.getElementById("myCanvas");
 		  }
 		  
 		  function drawBricks() {
-			for(var c=0; c<brickColumnCount; c++) {
-			  for(var r=0; r<brickRowCount; r++) {
-				if(bricks[c][r].status == 1) {
-				  var brickX = (c*(brickWidth+brickPadding))+brickOffsetLeft;
-				  var brickY = (r*(brickHeight+brickPadding))+brickOffsetTop;
-				  bricks[c][r].x = brickX;
-				  bricks[c][r].y = brickY;
-				  ctx.beginPath();
-				  ctx.rect(brickX, brickY, brickWidth, brickHeight);
-				  ctx.fillStyle = neon();
-				  ctx.shadowColor = '#fff';
-				  ctx.shadowBlur = 5;
-				  ctx.shadowOffsetX = 0;
-				  ctx.shadowOffsetY = 0;
-				  ctx.fill();
-				  
-				  
+			for (var c = 0; c < brickColumnCount; c++) {
+				for (var r = 0; r < brickRowCount; r++) {
+					if (bricks[c][r].status == 1) {
+						var brickX = (c * (brickWidth + brickPadding)) + brickOffsetLeft;
+						var brickY = (r * (brickHeight + brickPadding)) + brickOffsetTop;
+						bricks[c][r].x = brickX;
+						bricks[c][r].y = brickY;
+						ctx.beginPath();
+						ctx.rect(brickX, brickY, brickWidth, brickHeight);
+						ctx.fillStyle = neon();
+						ctx.shadowColor = '#fff';
+						ctx.shadowBlur = 5;
+						ctx.shadowOffsetX = 0;
+						ctx.shadowOffsetY = 0;
+						ctx.fill();
+						ctx.closePath();
+					}
 				}
-			  }
 			}
-		  }
+		}
+		
 		  
 
-		function collisionDetection() {
-			for(var c=0; c<brickColumnCount; c++) {
-				for(var r=0; r<brickRowCount; r++) {
+		  function collisionDetection() {
+			for (var c = 0; c < brickColumnCount; c++) {
+				for (var r = 0; r < brickRowCount; r++) {
 					var b = bricks[c][r];
-					if(b.status == 1) {
-						if(x > b.x && x < b.x+brickWidth && y > b.y && y < b.y+brickHeight) {
+					if (b.status == 1) {
+						if (x > b.x && x < b.x + brickWidth && y > b.y && y < b.y + brickHeight) {
 							dy = -dy;
 							b.status = 0;
 							score++;
-							if(score == brickRowCount*brickColumnCount) {
+							ballSpeed += 0.1; // Aumenta a velocidade da bola a cada colisão com um tijolo
+							if (score == brickRowCount * brickColumnCount) {
 								alert("YOU WIN, CONGRATULATIONS!");
 								document.location.reload();
 							}
@@ -95,7 +98,6 @@ var canvas = document.getElementById("myCanvas");
 				}
 			}
 		}
-
 		function drawScore() {
 			ctx.font = "16px Arial";
 			ctx.fillStyle = "#0095DD";
